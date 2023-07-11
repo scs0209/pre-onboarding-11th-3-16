b08cfd8cf208425f48c9da7804803c9317ef18e7
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-07-12 01:06:59 +0900

    feat: 인피니트 스크롤 구현

--- src/components/IssueList/IssueList.tsx
+++ src/components/IssueList/IssueList.tsx
@@ -1,23 +1,55 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { getIssues } from '@/api/github';
 import { useIssuesContext } from '@/context/IssueContext';
 
 import AdBanner from '../AdBanner/AdBanner';
-import IssueDetail from '../IssueDetail/IssueDetail';
 import IssueItem from './IssueItem';
 
 const IssueList = () => {
   const { issues, setIssues } = useIssuesContext();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+
+  const fetchData = async (query = '', page = 1) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const newIssues = await getIssues(query, page);
+      const updatedIssues = [...issues, ...newIssues];
+
+      setIssues(updatedIssues);
+    } catch (error: any) {
+      setError(error);
+    }
+    setLoading(false);
+  };
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+      document.documentElement.offsetHeight
+    )
+      return;
+    setPage((prevPage) => prevPage + 1);
+  };
 
   useEffect(() => {
-    async function fetchIssues() {
-      const fetchedIssues = await getIssues();
-
-      setIssues(fetchedIssues);
-    }
-    fetchIssues();
+    fetchData();
   }, []);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (page > 1) fetchData('', page);
+  }, [page]);
 
   return (
     <>
@@ -29,6 +61,8 @@ const IssueList = () => {
             {index === 4 && <AdBanner />}
           </li>
         ))}
+        {loading && <div>Loading...</div>}
+        {error && <div>Error: {error}</div>}
       </ul>
     </>
   );
