82eef5fe9669c36573713513f33c9c51b001dfc2
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-07-12 00:40:25 +0900

    feat: IssueList route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+
+import Header from './components/Header/Header';
 import IssueList from './components/IssueList/IssueList';
 import { IssuesProvider } from './context/IssueContext';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <IssuesProvider>
-        <IssueList />
+        <Routes>
+          <Route path="/" element={<IssueList />} />
+        </Routes>
       </IssuesProvider>
     </div>
   );
